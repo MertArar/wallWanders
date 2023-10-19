a5efbc6e3d8918bfecae1a5808aa06231a902daa
MertArar <mertarar.ma@gmail.com>
2023-10-19 14:28:37 +0300

    Coordinate system added into the tails

---
+++ Assets/Enemy/EnemyMover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] float waitTime = 1f; 
+    
+    void Start()
+    {
+        StartCoroutine(FollowPath()); 
+    }
+
+    IEnumerator FollowPath()
+    {
+        foreach(Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
---
+++ Assets/Tiles/CoordinateLabeler.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteAlways] 
+public class CoordinateLabeler : MonoBehaviour
+{
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+    void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        DisplayCoordinates();
+    }
+    void Update()
+    {
+        if(!Application.isPlaying)
+         {
+            DisplayCoordinates();
+            UpdateObjectName();
+         }
+    }
+    
+    void DisplayCoordinates()
+    {
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+        
+        label.text = coordinates.x + "," + coordinates.y;
+    }
+    
+    void UpdateObjectName()
+    {
+        transform.parent.name = coordinates.ToString();
+    }
+}
