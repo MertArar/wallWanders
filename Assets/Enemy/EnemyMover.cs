fae6b3922862a551aea7b1e73d23818bcf4aa329
MertArar <mertarar.ma@gmail.com>
2023-10-20 17:56:33 +0300

    Nested prefabs added

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f; 
+    [SerializeField] [Range(0f , 5f)] float speed = 1f; 
     
     void Start()
     {
@@ -15,9 +15,19 @@ public class EnemyMover : MonoBehaviour
     IEnumerator FollowPath()
     {
         foreach(Waypoint waypoint in path)
-        {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+        {   
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+            
+            transform.LookAt(endPosition);
+            
+            while(travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }  
         }
     }
 }
